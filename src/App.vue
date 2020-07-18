a885a0e9df4df6ec9a4d534e06774b17f064b416
Mihailo Pantovic <mihailo.pantovic@helbiz.com>
2020-07-18 10:22:15 +0200

    ADD: Project setup

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <l-form />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import LForm from './components/LForm'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    LForm
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
